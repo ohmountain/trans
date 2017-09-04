bd36263bb03461e904e181b1623a9879245e8097
renshan <1005110700@qq.com>
2017-09-04 16:40:49 +0800

    删除调试信息

--- src/NiwoBundle/Service/Zhimi.php
+++ src/NiwoBundle/Service/Zhimi.php
@@ -207,8 +207,6 @@ class Zhimi
                 "ret_code" => 500,
                 "value" => null,
                 "reason_string" => "注册请求网络错误",
-                "error_details" => $result->error_message,
-                "url" => $url
             ]));
 
             return $response;
