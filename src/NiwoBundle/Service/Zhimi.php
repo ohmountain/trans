acda45b5c1207e836366b401ad9516477206e793
renshan <1005110700@qq.com>
2017-09-16 15:37:04 +0800

    f

--- src/NiwoBundle/Service/Zhimi.php
+++ src/NiwoBundle/Service/Zhimi.php
@@ -1154,7 +1154,7 @@ class Zhimi
         foreach ($parameter["images"] as $k => $image) {
             $name = $hash ."_". $k;
 
-            if ($image) {
+            if (count($image) > 63) {
                 $image = "data:image/png;base64," . $image;
             }
 
