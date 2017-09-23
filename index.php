9e50c72d7d26da2ab588b7207cd1ab9432dec62b
renshan <1005110700@qq.com>
2017-09-23 20:11:28 +0800

    f

--- index.php
+++ index.php
@@ -104,4 +104,6 @@ $ownership = [
 ];
 
 
+
+http://localhost:8000/aabbcc?data={%22comm_name%22:%22\u6e05\u9547\u5e02\u536b\u57ce\u9547\u51e4\u5c71\u6751%22,%22comm_pp_name%22:%22\u9ad8\u5c71\u7ec4%22,%22owner_id%22:%2252018110221804003J%22,%22owner_name%22:%22\u6a0a\u7965\u534e%22,%22owner_gender%22:%22\u7537%22,%22owner_contact%22:%22%22,%22owner_sid%22:%22522502196501132652%22,%22family_name%22:%22\u6a0a\u9a84%22,%22family_gender%22:%22\u7537%22,%22family_sid%22:%22522502199203072634%22,%22relationship%22:%22\u7236\u5b50%22,%22block%22:[{%22block_name%22:%22\u571f\u4e95\u7530%22,%22block_area%22:%222.03%22,%22block_type%22:%22\u7530%22,%22block_no%22:%220013%22,%22block_cordinate%22:%22%20%22,%22block_shape%22:%22\u4e0d\u89c4\u5219%22,%22usage_status%22:%222%22,%22contract_id_hash%22:%22%22},{%22block_name%22:%22\u54e8\u6811\u811a\u571f%22,%22block_area%22:%220.81%22,%22block_type%22:%22\u571f%22,%22block_no%22:%220014%22,%22block_coordinate%22:%22%20%22,%22block_shape%22:%22\u4e0d\u89c4\u5219%22,%22usage_status%22:%222%22,%22contract_id_hash%22:%22%22}]}
 echo json_encode($ownership) . "\n";
